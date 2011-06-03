846de7866ec8e53eecbffa801a7b113378774a81
Markus Mannel <markus@Markus-Mannels-iMac.local>
2011-06-03 23:39:59 +0200

    Removed unneeded files

--- tr-game-server/src/crypto/trcrypt.h
+++ tr-game-server/src/crypto/trcrypt.h
@@ -3,7 +3,7 @@
  *  tr-auth-server
  *
  *  Created by Markus Mannel on 31.05.11.
- *  Copyright 2011 InfiniteRasa. All rights reserved.
+ *  Copyright 2011 Overheat. All rights reserved.
  *
  */
 
